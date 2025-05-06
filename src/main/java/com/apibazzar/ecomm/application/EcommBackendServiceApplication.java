f81265307c3dfe277e93577980cb4f58a94b0780
apibazzar <apibazzar.team@gmail.com>
2025-05-06 23:38:00 +0530

    Initial commit

---
+++ src/main/java/com/apibazzar/ecomm/application/EcommBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apibazzar.ecomm.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommBackendServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apibazzar/ecomm/application/EcommBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apibazzar.ecomm.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommBackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
